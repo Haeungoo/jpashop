c908e09af6f7b79e9c04e19d1bce644420a5480d
Haeun Goo <becauseiami98@gmail.com>
2021-09-02 10:40:47 +0900

    Initial commit

---
+++ src/test/java/jpastudy/jpashop/UserTest.java
@@ -0,0 +1,15 @@
+package jpastudy.jpashop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserTest {
+    @Test
+    public void user(){
+        User user = new User();
+        user.setName("boot");
+        assertEquals(user.getName(), "boot");
+    }
+
+}
